af8a25a37b361308b0d516bd36a1b9898f4d7f7d
Sri Topula <siri8691@gmail.com>
2021-12-26 16:53:30 +1100

    Used string array and for loop to read and save grocery names

---
+++ src/Grocery.Tracker.ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Grocery.Tracker.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] groceryNames = new string[100];
+            var k = 0;
+            for (int i = 0; i < groceryNames.Length; i++)
+            {
+                Console.WriteLine("Please enter grocery name:");
+                string groceryName = Console.ReadLine();
+                if (groceryName == "exit")
+                {
+                    k = i;
+                    break;
+                }
+                groceryNames[i] = groceryName;
+            }
+            PrintNames(groceryNames,k);
+        }
+
+        static void PrintNames(string[] items,int z)
+        {
+            for (int i = 0; i < z; i++)
+            {
+                Console.WriteLine(items[i]); 
+            }
+            
+        }
+    }
+}
